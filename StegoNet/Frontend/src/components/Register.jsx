70828aefd4b5353469f98bdfbc6038cb01df18c7
Pragnesh_Reddy <151606173+itspragnesh@users.noreply.github.com>
2025-08-13 00:54:25 +0530

    Update Register.jsx

--- StegoNet/Frontend/src/components/Register.jsx
+++ StegoNet/Frontend/src/components/Register.jsx
@@ -12,7 +12,7 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/register', { username, password });
+      await axios.post('https://stegonet-4.onrender.com/api/register', { username, password });
       setMessage('Registered successfully! Please login.');
       setTimeout(() => navigate('/login'), 2000);
     } catch (err) {
